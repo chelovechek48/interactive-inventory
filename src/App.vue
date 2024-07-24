0b25fc02531cb44f7257729d0ac8aaae36d18bb6
Maksim Ivanyukhin <chelovechek.maks@mail.ru>
2024-07-24 16:04:21 +0300

    Created page container

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,32 @@
+<script setup>
+
+</script>
+
 <template>
-  <main>
-    page
+  <main class="page">
+    <div class="page__container">
+      123
+    </div>
   </main>
 </template>
 
 <style lang="scss">
 @forward '@styles';
+@use '@vars/colors';
+@use '@vars/breakpoints' as *;
+
+.page {
+  background-color: colors.$background;
+  min-height: 100dvh;
+  padding: $container-padding;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &__container {
+    width: min(100vw, $container-width);
+    background-color: white;
+  }
+}
 </style>
